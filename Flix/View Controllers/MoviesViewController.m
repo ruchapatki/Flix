74b04b3339a760b2e5f14f237f9a15cb523dfefd
Rucha Patki <ruchapatki@fb.com>
2018-06-28 10:49:53 -0700

    Fixed activing indicator issue and added alert when networking error occurs

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -30,13 +30,11 @@
     [self.activityIndicator startAnimating];
 }
 
-- (void)viewDidAppear:(BOOL)animatedP{
-    [super viewDidAppear:YES];
-    //TODO: Fix this!!!
-    // Stop the activity indicator
-    // Hides automatically if "Hides When Stopped" is enabled
-    [self.activityIndicator stopAnimating];
-}
+//- (void)viewDidAppear:(BOOL)animatedP{
+//    [super viewDidAppear:YES];
+//    //TODO: Fix this!!!
+//
+//}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -54,16 +52,30 @@
 }
 
 - (void)fetchMovies {
-    
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         //this part runs when network call is finished
         if (error != nil) {
+            //creating alert if networking error
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies" message:@"The internet connection appears to be offline." preferredStyle:(UIAlertControllerStyleAlert)];
+            
+            // create try again action
+            UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                // handle tryAgain response here. Doing nothing will dismiss the view.
+                [self fetchMovies];
+            }];
+            [self presentViewController:alert animated:YES completion:^{
+                // optional code for what happens after the alert controller has finished presenting
+            }];
+            // add the tryAgain action to the alertController
+            [alert addAction:tryAgainAction];
+
             NSLog(@"%@", [error localizedDescription]);
         }
         else {
+            
             NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
             
             NSLog(@"%@", dataDictionary);
@@ -75,6 +87,10 @@
             
             //refreshes, will call numberOfRowsInSection again
             [self.tableView reloadData];
+            // Stop the activity indicator
+            // Hides automatically if "Hides When Stopped" is enabled
+            [self.activityIndicator stopAnimating];
+            
         }
         [self.refreshControl endRefreshing];
     }];
@@ -92,7 +108,7 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     //indexpath has rows and sections
-    MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
+    MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell" forIndexPath:indexPath];
     
     NSDictionary *movie = self.movies[indexPath.row];
     cell.titleLabel.text = movie[@"title"];
